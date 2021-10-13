5f6d1c5f857aec94d29bd06b8402b53200cceed5
zlsong <53231273+ZL-Song@users.noreply.github.com>
2021-10-12 22:29:33 -0500

    Antoine constant converter.
    A simple program to convert parameters from Antoine Equations of different units. 
    Note that A, B, C values can be different when T, P is in different unit.

---
+++ AntnCnvt/AntnCnvt/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace AntnCnvt {
+    /// <summary>
+    /// App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App : Application {
+    }
+}
